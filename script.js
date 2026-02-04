32fa3f9e207d89639ccf2f7244bdb2862977ce90
Vikramgupte99 <vikramgupte99@gmail.com>
2026-02-04 02:16:19 -0500

    Created function for timer with 10 seconds countdown when user clicks start button.Also added a function input which compares value of its input and equation value. If input and random equation value is same it adds 10 pts to score.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var firstNum;
 var secondNum;
+var seconds = 10;
+var score = 0;
+var highScore = 0;
+var timer = null;
+var equationValue;
 
 //function for equation
 $(document).on('click','#startBtn',function () {
@@ -8,5 +13,45 @@ $(document).on('click','#startBtn',function () {
     secondNum = Math.floor(Math.random() * 10) + 1;
     console.log(firstNum);
     console.log(secondNum);
+    equationValue = firstNum + secondNum;
     $('.equation').html(firstNum + ' ' + '+' + ' ' + secondNum);
+    countdown();
+    inputFunc();
 });
+
+//function for timer
+var countdown = function () {
+    $('.counter').html(seconds  + ' seconds left');
+
+    if(!timer) {
+        timer = setInterval(function () {
+            seconds--;
+            $('.counter').html(seconds  + ' seconds left');
+
+            if(seconds <= 0) {
+                clearInterval(timer);
+                timer = null;
+                seconds = 0;
+                console.log(seconds);
+                $('.equation').html("GAME OVER");
+                $('.counter').html("Time over");
+            }
+        },1000);
+    }
+}
+
+//function for input
+var inputFunc = function () {
+    $('.input').keyup(function (e) {
+    e.preventDefault();
+    
+    var inputVal = Number($('.input').val());
+    if(inputVal === equationValue) {
+        score += 10;
+        $('.score span').html(score);
+        seconds++;
+        $('.counter').html(seconds  + ' seconds left');
+        $('input').val('');
+    }
+})
+};
