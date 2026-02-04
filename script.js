caacce82e708138dd40772488f884333a68f5af3
Vikramgupte99 <vikramgupte99@gmail.com>
2026-02-03 21:23:27 -0500

    Created function to test working of 'Start' button and to create an equation with two random numbers with '+' operator. Also injected equation in .equation element to display on click.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var pText = $('p').text();
-console.log(pText);
-$('p').click(function () {
-    $(this).addClass('color');
-})
+var firstNum;
+var secondNum;
+
+//function for equation
+$(document).on('click','#startBtn',function () {
+    console.log('Start button working');
+    firstNum = Math.floor(Math.random() * 10) + 1;
+    secondNum = Math.floor(Math.random() * 10) + 1;
+    console.log(firstNum);
+    console.log(secondNum);
+    $('.equation').html(firstNum + ' ' + '+' + ' ' + secondNum);
+});
